3ae6ef652fda887465ff9f56d82595b752bb569b
Ramitha Heshan <ramithacampus@gmail.com>
2025-06-09 23:55:17 +0530

    update

---
+++ Python/Day 01/Task2.py
@@ -0,0 +1,7 @@
+x = input("Input Number 1: ")
+y = input("Input Number 2: ")
+z = input("Input Number 3: ")
+
+result = int(x) * int(y) * int(z)
+
+print("The product of the three numbers is: " + str(result))
