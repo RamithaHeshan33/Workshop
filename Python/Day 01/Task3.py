959819d7a9c28f6bbf6b8da4e0920d3f3f94db0c
Ramitha Heshan <ramithacampus@gmail.com>
2025-06-10 00:03:08 +0530

    task 3

---
+++ Python/Day 01/Task3.py
@@ -0,0 +1,11 @@
+Maths = int(input("Input Maths Marks: "))
+English = int(input("Input English Marks: "))
+Science = int(input("Input Science Marks: "))
+History = int(input("Input History Marks: "))
+ICT = int(input("Input ICT Marks: "))
+
+Total = Maths + English + Science + History + ICT
+Average = Total / 5
+
+print("Total Marks: " + str(Total))
+print("Average Marks: " + str(Average))
