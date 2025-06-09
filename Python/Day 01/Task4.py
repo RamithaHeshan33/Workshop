280e11460a244c94dcfa399326dde14813011c4b
Ramitha Heshan <ramithacampus@gmail.com>
2025-06-10 00:05:42 +0530

    task 4

---
+++ Python/Day 01/Task4.py
@@ -0,0 +1,9 @@
+num1 = input("Input Number 1: ")
+num2 = input("Input Number 2: ")
+num3 = input("Input Number 3: ")
+
+total = int(num1) + int(num2) + int(num3)
+average = total / 3
+
+print("Total: " + str(total))
+print("Average: 10" + str(average))
